5fddcbc585f71bcdd51c294b80ec7d0ebbcc338f
mian <programerx55@gmail.com>
2025-02-16 01:03:48 +0500

    changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,16 @@
+
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("org.sonarqube") version "6.0.1.5171"
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "MTprogramer_Testing_U")
+        property("sonar.organization", "mtprogramer")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
